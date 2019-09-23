23e107e3b6c021b6b3ed36c6791791032fbd4c25
Soumya_Goyal <somyagoyal739@gmail.com>
2019-09-23 11:52:00 -0700

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+    attr_accessor :title, :author
+ 
+    def title=(title)
+        @title = title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+        
+end
